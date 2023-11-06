eb89d57ac8ab75f37117ed47ffd2ff56893aba32
Tom Palmer <remlapmot@hotmail.com>
2023-11-06 16:14:19 +0000

    Comment out separate html

--- render.R
+++ render.R
@@ -1,14 +1,14 @@
 # Successful render under knitr 1.44 
 devtools::install_version("knitr", version = "1.44", repos = "https://cran.rstudio.com")
-rmarkdown::render("test.Rmd", output_file = "test-knitr-1.44.html")
+# rmarkdown::render("test.Rmd", output_file = "test-knitr-1.44.html")
 rmarkdown::render("test.Rmd", output_file = "test-knitr-1.44.md", output_format = "github_document")
 
 # Error under knitr 1.45
 devtools::install_version("knitr", version = "1.45", repos = "https://cran.rstudio.com")
-rmarkdown::render("test.Rmd", output_file = "test-knitr-1.45.html")
+# rmarkdown::render("test.Rmd", output_file = "test-knitr-1.45.html")
 rmarkdown::render("test.Rmd", output_file = "test-knitr-1.45.md", output_format = "github_document")
 
 # Test under development version
 remotes::install_github("yihui/knitr")
-rmarkdown::render("test.Rmd", output_file = "test-knitr-devel.html")
+# rmarkdown::render("test.Rmd", output_file = "test-knitr-devel.html")
 rmarkdown::render("test.Rmd", output_file = "test-knitr-devel.md", output_format = "github_document")
