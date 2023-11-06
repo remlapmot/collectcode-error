7e3d466f703e94f421843aa5bc5080b5e7d35055
Tom Palmer <remlapmot@hotmail.com>
2023-11-06 15:41:28 +0000

    Change output filename

--- render.R
+++ render.R
@@ -4,7 +4,7 @@ rmarkdown::render("test.Rmd", output_file = "test-knitr-1.44.html")
 
 # Error under knitr 1.45
 devtools::install_version("knitr", version = "1.45", repos = "https://cran.rstudio.com")
-rmarkdown::render("test.Rmd", output_file = "index.html")
+rmarkdown::render("test.Rmd", output_file = "test-knitr-1.45.html")
 
 # Test under development version
 remotes::install_github("yihui/knitr")
