473d850e8132aeceae34cc909b9b9f21bbc520fd
Tom Palmer <remlapmot@hotmail.com>
2023-11-06 15:27:11 +0000

    Initial commit

---
+++ example.R
@@ -0,0 +1,11 @@
+# Successful render under knitr 1.44 
+devtools::install_version("knitr", version = "1.44", repos = "https://cran.rstudio.com")
+rmarkdown::render("test.Rmd", output_file = "test-knitr-1.44.html")
+
+# Error under knitr 1.45
+devtools::install_version("knitr", version = "1.45", repos = "https://cran.rstudio.com")
+rmarkdown::render("test.Rmd", output_file = "index.html")
+
+# Test under development version
+remotes::install_github("yihui/knitr")
+rmarkdown::render("test.Rmd", output_file = "test-knitr-devel.html")
