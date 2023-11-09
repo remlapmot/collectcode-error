feb9756e4928c0e713bb701ff1e790ad7449581c
Tom Palmer <remlapmot@hotmail.com>
2023-11-09 14:53:35 +0000

    Add a second existing profile.do example

--- test2/profile.do
+++ test2/profile.do
@@ -1,3 +1,2 @@
-
 display "Example with an existing profile.do"
 display 1 + 1
--- test2/render.R
+++ test2/render.R
@@ -1,3 +1,7 @@
 setwd(here::here("test2"))
 rstudioapi::restartSession()
 rmarkdown::render("test2.Rmd", output_file = "test2-knitr-commits-5.md", output_format = "github_document")
+
+remotes::install_version('knitr', '1.44')
+rstudioapi::restartSession()
+rmarkdown::render("test2.Rmd", output_file = "test2-knitr-1-44.md", output_format = "github_document")
